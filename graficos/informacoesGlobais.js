b94fcfec04bd92261ec69ef0f007ff7df75433c4
Eric Henrique Alves Vicente <106555851+VicenteEric@users.noreply.github.com>
2024-09-18 11:01:47 -0300

    gilmar

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -7,6 +7,7 @@ async function vizualizarInformacoesGlobais() {
     const pessoasNoMundo = (dados.total_pessoas_mundo / 1e9)
     const horas = parseInt(dados.tempo_medio)
     const minutos = Math.round(dados.tempo_medio - horas) * 100
+    const porcentagemConectada = ((pessoasConectadas / pessoasNoMundo) * 100).toFixed(2)
 
 
     const paragrafo = document.createElement('p')
@@ -14,7 +15,8 @@ async function vizualizarInformacoesGlobais() {
     paragrafo.innerHTML = `Você sabia que o mundo 
     tem <span>${pessoasNoMundo} bilhões</span> de pessoas e que
     aproximadamente <span>${pessoasConectadas} bilhões</span> estão conectadas em alguma rede social e 
-    passam em média <span>${horas} horas</span> e <span>${minutos} minutos </span> conectadas.`
+    passam em média <span>${horas} horas </span> e <span>${minutos} minutos </span> conectadas.<br>Isso significa que aproximadamente 
+    ${porcentagemConectada}% de pessoas estão conectadas em alguma rede social`
 
 
     const container = document.getElementById('graficos-container')
