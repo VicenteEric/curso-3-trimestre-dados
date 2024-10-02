3e2d27b861370ff18ab0c5d3459f0e58a2ebf24c
Eric Henrique Alves Vicente <106555851+VicenteEric@users.noreply.github.com>
2024-10-02 19:42:37 -0300

    adadad

--- graficos/quantitadeUsuarios.js
+++ graficos/quantitadeUsuarios.js
@@ -18,42 +18,41 @@ async function quantidadeUsuariosPorRede() {
         }
     ]
 
-    const laytout = {
-        plot_bgcolor: getCSS('--bg-color'),
-        paper_bgcolor: getCSS('--bg-color'),
-        title: {
-            text: 'Redes sociais com mais usuários',
-            x: 0,
-            font: {
-                color: getCSS('--primary-color'),
-                size: 30,
-                font: getCSS('--font')
-            }
-        },
-        xaxis: {
-            tickfont: tickConfig,
-            title: {
-                text: 'Nome das redes',
-                font: {
-                    color: getCSS('--secondary-color')
-                }
-            }
-        },
-        yaxis: {
-            tickfont: tickConfig,
-            title: {
-                text: 'Bilhões de usuários ativos',
-                font: {
-                    color: getCSS('--secondary-color')
-                }
-            }
-        }
-    }
 
     const grafico = document.createElement('div')
     grafico.className = 'grafico'
     document.getElementById('graficos-container').appendChild(grafico)
-    Plotly.newPlot(grafico, data, laytout)
+    Plotly.newPlot(grafico, data, {
+            plot_bgcolor: getCSS('--bg-color'),
+            paper_bgcolor: getCSS('--bg-color'),
+            title: {
+                text: 'Redes sociais com mais usuários',
+                x: 0,
+                font: {
+                    color: getCSS('--primary-color'),
+                    size: 30,
+                    font: getCSS('--font')
+                }
+            },
+            xaxis: {
+                tickfont: tickConfig,
+                title: {
+                    text: 'Nome das redes',
+                    font: {
+                        color: getCSS('--secondary-color')
+                    }
+                }
+            },
+            yaxis: {
+                tickfont: tickConfig,
+                title: {
+                    text: 'Bilhões de usuários ativos',
+                    font: {
+                        color: getCSS('--secondary-color')
+                    }
+                }
+            }
+        })
 }
 
 quantidadeUsuariosPorRede()
