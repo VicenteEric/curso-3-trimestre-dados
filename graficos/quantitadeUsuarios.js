a37ecddcbbe7b12715851c425354d87addc73ff5
Eric Henrique Alves Vicente <106555851+VicenteEric@users.noreply.github.com>
2024-10-02 11:09:30 -0300

    confere

--- graficos/quantitadeUsuarios.js
+++ graficos/quantitadeUsuarios.js
@@ -1,5 +1,3 @@
-import { getCSS, tickConfig } from "./common.js"
-
 async function quantidadeUsuariosPorRede() {
     const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json'
     const res = await fetch(url)
@@ -11,49 +9,14 @@ async function quantidadeUsuariosPorRede() {
         {
             x: nomeDasRedes, 
             y: quantidadeDeUsuarios, 
-            type: 'bar',
-            marker: {
-                color: getCSS('--primary-color')
-            }
+            type: 'bar'
         }
     ]
-
-    const laytout = {
-        plot_bgcolor: getCSS('--bg-color'),
-        paper_bgcolor: getCSS('--bg-color'),
-        title: {
-            text: 'Redes sociais com mais usuários',
-            x: 0,
-            font: {
-                color: getCSS('--primary-color'),
-                size: 30,
-                font: getCSS('--font')
-            }
-        },
-        xaxis: {
-            tickfont: tickConfig,
-            title: {
-                text: 'Nome das redes',
-                font: {
-                    color: getCSS('--secondary-color')
-                }
-            }
-        },
-        yaxis: {
-            tickfont: tickConfig,
-            title: {
-                text: 'Bilhões de usuários ativos',
-                font: {
-                    color: getCSS('--secondary-color')
-                }
-            }
-        }
-    }
 
     const grafico = document.createElement('div')
     grafico.className = 'grafico'
     document.getElementById('graficos-container').appendChild(grafico)
-    Plotly.newPlot(grafico, data, laytout)
+    Plotly.newPlot(grafico, data)
 }
 
 quantidadeUsuariosPorRede()
